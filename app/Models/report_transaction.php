913fbac65cb8873c9b8ed43ce0d46c0df7468fb6
DianHakim <ffmolen803@gmail.com>
2025-11-04 20:25:36 +0700

    Initial commit

---
+++ app/Models/report_transaction.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class report_transaction extends Model
+{
+    /** @use HasFactory<\Database\Factories\ReportTransactionFactory> */
+    use HasFactory;
+}
